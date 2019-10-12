1d4c1f6a288a1a23da8d92ba24cf59f57350f2d3
JunlinXu <47915065+JunlinXu@users.noreply.github.com>
2019-10-12 17:02:52 +0800

    Update main_mc.m

--- main_mc.m
+++ main_mc.m
@@ -5,6 +5,10 @@
 % iter Number of iterations
 %lam1,lam2,lam3: parameters
 %tol:Threshold of convergence
+%recMatrix:obtain the prediction score matrix
+load interaction;
+load sd;
+load sm;
 norm=1;
 rank=3;
 iter=60;
@@ -13,10 +17,10 @@ lam1=0.1;
 lam2=1;
 lam3=1;
 [m,n]=size(interaction);
-    Map=MAPS(interaction);
-    I11=getAdjMA(interaction,Map);
-    [X S Y dist]=OptSpaceII(I11,sd,sm,rank,iter,tol,lam1,lam2,lam3);
-    recMatrix = X*S*Y';
+Map=MAPS(interaction);
+I11=getAdjMA(interaction,Map);
+[X S Y dist]=OptSpaceII(I11,sd,sm,rank,iter,tol,lam1,lam2,lam3);
+recMatrix = X*S*Y';
     
 
 
